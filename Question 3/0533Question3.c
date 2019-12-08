2e26e1e288e1876901b405d2ad47aa4e8d78ebac
Ahmed Mahmoud mohammed <39026838+Aeroahmed95@users.noreply.github.com>
2019-12-08 11:34:24 +0200

    Update 0533Question3.c

--- Question 3/0533Question3.c
+++ Question 3/0533Question3.c
@@ -1,18 +1,44 @@
-/*
- * 0533Question3.c
- *
- *  Created on: Dec 7, 2019
- *      Author: MASTER
- */
+#include "Question.h"
 
-void stringRearaange(char names[])
+// Complete the stringRearrange function below.
+
+// Please either make the string static or allocate on the heap. For example,
+// static char str[] = "hello world";
+// return str;
+//
+// OR
+//
+// char* str = "hello world";
+// return str;
+//
+
+/* IMPORTANT NOTE:	DO NOT use printf and/or scanf in this file*/
+char* stringRearrange(char* arr)
 {
-	int i=0,count=0;
-	char each[][];
-	while(names[i] != '\0')
-	{
-		if(names[i]==',')count++;
-	}
-	count++;
+	//	Write your code here
+	int i=0,j=0,count=0;
+		char each [100][100],temp[100];
+		while(names[i] != '\0')
+		{
+			if(names[i]==',')
+				{
+					count++;
+					j++;
+				}
+			each[j][i]=names[i];
+			i++;
+		}
+		count++;
+		for(int k=0;k<=count;k++)
+			for(int w=i+1;w<=count;w++)
+			{
+				if(strcmp(each[i],each[j])>0)
+				{
+					strcpy(temp,each[i]);
+					strcpy(each[i],each[j]);
+					strcpy(each[j],temp);
+				}
+			}
 
 }
+
