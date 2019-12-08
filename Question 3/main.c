70ece66749d94b7efe8a0c8ba00f978445bea5ce
Ahmed Mahmoud mohammed <39026838+Aeroahmed95@users.noreply.github.com>
2019-12-08 14:08:46 +0200

    Update main.c

--- Question 3/main.c
+++ Question 3/main.c
@@ -1,16 +1,16 @@
 /*
  * This file is a test case you may use and change to check your code
  */
-#include "Question.h"
+#include "stringRearrange.h"
+
 
 int main()
 {
-	char* result;
-	
-	char* arr = "Ahmed,Omar,Maged,Mohanad,Gamal,Mohammed";
-
+    
+    char* result;
+	char arr[] = "Ahmed,Omar,Maged,Mohanad,Gamal,Mohammed";
+    printf("the old order was :: %s\n", arr);
 	result = stringRearrange(arr);
-    printf("%s", result);	// The result Will be "Ahmed,Gamal,Maged,Mohammed,Mohanad,Omar"
-	
+    printf("the new order is  :: %s", result);
     return 0;
 }
